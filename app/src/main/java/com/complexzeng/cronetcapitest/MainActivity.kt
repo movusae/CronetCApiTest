45ff706939de7aed807316e1eabedcf7b2d1fb7f
complexzeng <complexzeng@tencent.com>
2023-10-29 20:29:23 +0800

    init

---
+++ app/src/main/java/com/complexzeng/cronetcapitest/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.complexzeng.cronetcapitest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.complexzeng.cronetcapitest.databinding.ActivityMainBinding
+import org.chromium.net.CronetEngine
+
+class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    // Example of a call to a native method
+    binding.sampleText.text = stringFromJNI()
+    val cronet = CronetEngine.Builder(this).build()
+    Log.i("cplx", "cronet version=" + cronet.versionString)
+  }
+
+  /**
+   * A native method that is implemented by the 'cronetcapitest' native library,
+   * which is packaged with this application.
+   */
+  external fun stringFromJNI(): String
+
+  companion object {
+    // Used to load the 'cronetcapitest' library on application startup.
+    init {
+      System.loadLibrary("cronetcapitest")
+    }
+  }
+}
